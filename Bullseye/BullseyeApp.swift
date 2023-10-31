2f575f4bd7a5337e34444218e6357a234ce8e3f3
narcis dpr <nargesdeypir@gmail.com>
2023-10-31 19:30:48 +0330

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Narges on 31.10.23.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
